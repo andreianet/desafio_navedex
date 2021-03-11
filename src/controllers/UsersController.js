ae4f4313baf74b17e5eeabdb95cb40edc8a884fa
andreianet <andreia.netsys@hotmail.com>
2021-03-10 21:44:19 -0300

    implementado bcrypt

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,19 +1,22 @@
 const Users = require('../model/users');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-exports.signup = async (req, res) => {    
+exports.signup = async (req, res) => {  
+    
+    console.log(bcrypt);
     if(!req.body){
         res.status(400).send({
             message: "Content can not be empty"
         })
     } 
-    //create users   
-    const salt = await bcrypt.genSalt(8)
-    const passhash =  bcrypt.hashSync(req.body.senha, salt); //ERROR AQUI   
+    //create users 
+    var salt = await bcrypt.genSaltSync(10);
+    var hashedPassword  = await bcrypt.hashSync(req.body.senha.toString(), salt);;
+
+
     const users = new Users({        
         email: req.body.email,
-        senha: result.passhash
+        senha:hashedPassword 
     })   
     //save users    
     Users.signup(users,(err, data) => {
--- src/model/users.js
+++ src/model/users.js
@@ -1,18 +1,11 @@
 const con = require('./db.js');
-//const bcrypt = require('bcrypt');
+//const hash = require('bcrypt');
 
 
 const Users = function(user) {
     this.email = user.email;
     this.senha = user.senha;
 };
-
-/*Users.pre('signup', async function(next) {
-    const passHash = await bcrypt.hash(this.senha,8)
-    this.senha = passHash;
-
-    next();
-})*/
 
 Users.signup = (newUsers, result) => {
     con.query("INSERT INTO users SET ? ", newUsers,(err, res) => {
@@ -26,4 +19,4 @@ Users.signup = (newUsers, result) => {
     })
 }
 
-module.exports = Users;
+module.exports = Users
